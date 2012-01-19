4f97c8a6460b16a2260c76638f7d4fc1042e9bc1
Pyrolistical <pyrogx1133@gmail.com>
2012-01-19 01:56:45 -0800

    implemented pin configuration by picture! currently only prints messages out for testing. ie. keyboard is non-functional still need to work out many hardcoded issues

--- fourway/config.h
+++ fourway/config.h
@@ -20,6 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* controller configuration */
 #include "controller_teensy.h"
+#include "ports.h"
 
 #define LAYERS \
     /* Layer 0: Default Layer \
@@ -46,15 +47,30 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     { R1C0, R1C1 } \
 }
 
+/* Set 0 if need no debouncing */
+#define DEBOUNCE    5
+
+#define DIODE_DIRECTION ROWS
+
+#define PORTMAP \
+        /* GND-----Vcc */ \
+    C0, /* B0   T   F0 */ NC, \
+    C1, /* B1   E   F1 */ NC, \
+    NC, /* B2   E   F4 */ NC, \
+    NC, /* B3   N   F5 */ NC, \
+    NC, /* B7   S   F6 */ NC, \
+    R0, /* D0   Y   F7 */ NC, \
+    R1, /* D1       B6 */ NC, \
+    NC, /* D2   2   B5 */ NC, \
+    NC, /* D3   .   B4 */ NC, \
+    NC, /* C6   0   D7 */ NC, \
+    NC, /* C7-------D6 */ NC
+
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0xBEE0
 #define MANUFACTURER    t.m.k.
 #define PRODUCT         Macway mod
 #define DESCRIPTION     t.m.k. keyboard firmware for Fourway mod
-
-
-/* Set 0 if need no debouncing */
-#define DEBOUNCE    5
 
 
 /* key combination for command */
--- fourway/matrix.c
+++ fourway/matrix.c
@@ -26,13 +26,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "debug.h"
 #include "util.h"
 #include "matrix.h"
+#include "ports.h"
 
-
+// need to fix the row/col limit to support full C(22,2)
 #if (MATRIX_COLS > 16)
 #   error "MATRIX_COLS must not exceed 16"
 #endif
-#if (MATRIX_ROWS > 255)
-#   error "MATRIX_ROWS must not exceed 255"
+#if (MATRIX_ROWS > 8)
+#   error "MATRIX_ROWS must not exceed 8"
 #endif
 
 
@@ -40,6 +41,35 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #   define DEBOUNCE	0
 #endif
 static uint8_t debouncing = DEBOUNCE;
+
+static const bool matrix_has_diodes_in_row = DIODE_DIRECTION;
+
+
+// Maps the switch matrix to ports on the teensy
+// The index of portmap is teensy's port reading from left to right:
+/* GND-----Vcc */
+/* B0   T   F0 */
+/* B1   E   F1 */
+/* B2   E   F4 */
+/* B3   N   F5 */
+/* B7   S   F6 */
+/* D0   Y   F7 */
+/* D1       B6 */
+/* D2   2   B5 */
+/* D3   .   B4 */
+/* C6   0   D7 */
+/* C7-------D6 */
+/* Or in other words portmap is:
+ * Index:  0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21
+ * Port:  B0 F0 B1 F1 B2 F4 B3 F5 B7 F6 D0 F7 D1 B6 D2 B5 D3 B4 C6 D7 C7 D6
+ */
+static const uint8_t portmap[] = {
+    PORTMAP
+};
+
+static const uint8_t pinmap[] = {
+	0, 0, 1, 1, 2, 4, 3, 5, 7, 6, 0, 7, 1, 6, 2, 5, 3, 4, 6, 7, 7, 6
+};
 
 // matrix state buffer(1:on, 0:off)
 #if (MATRIX_COLS <= 8)
@@ -57,9 +87,15 @@ static uint16_t _matrix1[MATRIX_ROWS];
 #ifdef MATRIX_HAS_GHOST
 static bool matrix_has_ghost_in_row(uint8_t row);
 #endif
-static uint8_t read_col(void);
-static void unselect_rows(void);
-static void select_row(uint8_t row);
+#if (MATRIX_COLS <= 8)
+static uint8_t read(void);
+#else
+static uint16_t read(void);
+#endif
+static uint8_t read_pin(uint8_t index);
+static void unselect(void);
+static void unselect_pin(uint8_t index);
+static void select(uint8_t index);
 
 
 inline
@@ -74,17 +110,81 @@ uint8_t matrix_cols(void)
     return MATRIX_COLS;
 }
 
+#define CLEAR_PORT(index) (|= (1 << pinmap[index]));
+#define SET_PORT(index) (&= ~(1 << pinmap[index]));
+#define IS_INPUT(port) ((matrix_has_diodes_in_row && IS_COL(port)) || (!matrix_has_diodes_in_row && IS_ROW(port)))
+#define IS_OUTPUT(port) ((matrix_has_diodes_in_row && IS_ROW(port)) || (!matrix_has_diodes_in_row && IS_COL(port)))
+
+// Input with pull-up(DDR:0, PORT:1)
+void pull_up(uint8_t index)
+{
+	switch (index) {
+		case 0:
+		case 2:
+		case 4:
+		case 6:
+		case 8:
+		case 13:
+		case 15:
+		case 17:
+			print("pull up port B pin ");
+			phex(pinmap[index]);
+			print("\n");
+			//DDRB CLEAR_PORT(index);
+			//PORTB SET_PORT(index);
+			break;
+		case 18:
+		case 20:
+			print("pull up port C pin ");
+			phex(pinmap[index]);
+			print("\n");
+			//DDRC CLEAR_PORT(index);
+			//PORTC SET_PORT(index);
+			break;
+		case 10:
+		case 12:
+		case 14:
+		case 16:
+		case 19:
+		case 21:
+			print("pull up port D pin ");
+			phex(pinmap[index]);
+			print("\n");
+			//DDRD CLEAR_PORT(index);
+			//PORTD SET_PORT(index);
+			break;
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 9:
+		case 11:
+			print("pull up port F pin ");
+			phex(pinmap[index]);
+			print("\n");
+			//DDRF CLEAR_PORT(index);
+			//PORTF SET_PORT(index);
+			break;
+	}
+}
+
+
 void matrix_init(void)
 {
-    // initialize row and col
-    unselect_rows();
-    // Input with pull-up(DDR:0, PORT:1)
-    DDRB = 0x00;
-    PORTB = 0xFF;
+	_delay_ms(1000);
+	print_enable = true;
+	print("init matrix\n");
+	// initialize row and col
+    unselect();
+	for (uint8_t i = 0; i < 22; i++) {
+		if (IS_INPUT(portmap[i])) {
+			pull_up(i);
+		}
+	}
 
     // initialize matrix state: all keys off
-    for (uint8_t i=0; i < MATRIX_ROWS; i++) _matrix0[i] = 0x00;
-    for (uint8_t i=0; i < MATRIX_ROWS; i++) _matrix1[i] = 0x00;
+    for (uint8_t i = 0; i < MATRIX_ROWS; i++) _matrix0[i] = 0x00;
+    for (uint8_t i = 0; i < MATRIX_ROWS; i++) _matrix1[i] = 0x00;
     matrix = _matrix0;
     matrix_prev = _matrix1;
 }
@@ -97,19 +197,27 @@ uint8_t matrix_scan(void)
         matrix = tmp;
     }
 
-    for (uint8_t i = 0; i < MATRIX_ROWS; i++) {
-        unselect_rows();
-        select_row(i);
-        _delay_us(30);  // without this wait read unstable value.
-        if (matrix[i] != (uint8_t)~read_col()) {
-            matrix[i] = (uint8_t)~read_col();
-            if (debouncing) {
-                debug("bounce!: "); debug_hex(debouncing); print("\n");
-            }
-            debouncing = DEBOUNCE;
-        }
+	uint8_t row = 0;
+	for (uint8_t i = 0; i < 22; i++) {
+		if (IS_OUTPUT(portmap[i])) {
+			select(i);
+			_delay_ms(300);  // without this wait read unstable value.
+#if (MATRIX_COLS <= 8)
+			uint8_t stripe = ~read();
+#else
+			uint16_t stripe = ~read();
+#endif
+			if (matrix[row] != stripe) {
+				matrix[row] = stripe;
+				row++;
+				if (debouncing) {
+					debug("bounce!: "); debug_hex(debouncing); print("\n");
+				}
+				debouncing = DEBOUNCE;
+			}
+			unselect_pin(i);
+		}
     }
-    unselect_rows();
 
     if (debouncing) {
         debouncing--;
@@ -206,66 +314,189 @@ static bool matrix_has_ghost_in_row(uint8_t row)
 }
 #endif
 
-inline
-static uint8_t read_col(void)
+uint8_t read_pin(uint8_t index)
 {
-    return PINB;
+	switch (index) {
+		case 0:
+		case 2:
+		case 4:
+		case 6:
+		case 8:
+		case 13:
+		case 15:
+		case 17:
+			print("read port B pin ");
+			phex(pinmap[index]);
+			print("\n");
+			break;
+			//return PINB & (1 << pinmap[index]);
+		case 18:
+		case 20:
+			print("read port C pin ");
+			phex(pinmap[index]);
+			print("\n");
+			break;
+			//return PINC & (1 << pinmap[index]);
+		case 10:
+		case 12:
+		case 14:
+		case 16:
+		case 19:
+		case 21:
+			print("read port D pin ");
+			phex(pinmap[index]);
+			print("\n");
+			break;
+			//return PIND & (1 << pinmap[index]);
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 9:
+		case 11:
+			print("read port F pin ");
+			phex(pinmap[index]);
+			print("\n");
+			break;
+			//return PINF & (1 << pinmap[index]);
+	}
+	return 0;
 }
 
-inline
-static void unselect_rows(void)
+//inline
+#if (MATRIX_COLS <= 8)
+static uint8_t read(void)
+#else
+static uint16_t read(void)
+#endif
 {
-    // Hi-Z(DDR:0, PORT:0) to unselect
-    DDRC  &= ~0b11000000; // PC: 7,6
-    PORTC &= ~0b11000000;
-    DDRD  &= ~0b11000111; // PD: 7,6,2,1,0
-    PORTD &= ~0b11000111;
-    DDRF  &= ~0b11000000; // PF: 7,6
-    PORTF &= ~0b11000000;
+#if (MATRIX_COLS <= 8)
+	uint8_t row = 0xFF;
+#else
+	uint16_t row = 0xFFFF;
+#endif
+	uint8_t bit = 0;
+	for (uint8_t i = 0; i < 22; i++) {
+		if (IS_INPUT(portmap[i])) {
+			row |= ((read_pin(i) & 0x01) << bit);
+			bit++;
+		}
+	}
+	return row;
 }
 
-inline
-static void select_row(uint8_t row)
+// Hi-Z(DDR:0, PORT:0) to unselect
+void unselect_pin(uint8_t index)
 {
-    // Output low(DDR:1, PORT:0) to select
-    // row: 0    1    2    3    4    5    6    7    8
-    // pin: PD0, PC7, PD7, PF6, PD6, PD1, PD2, PC6, PF7
-    switch (row) {
-        case 0:
-            DDRD  |= (1<<0);
-            PORTD &= ~(1<<0);
-            break;
-        case 1:
-            DDRC  |= (1<<7);
-            PORTC &= ~(1<<7);
-            break;
-        case 2:
-            DDRD  |= (1<<7);
-            PORTD &= ~(1<<7);
-            break;
-        case 3:
-            DDRF  |= (1<<6);
-            PORTF &= ~(1<<6);
-            break;
-        case 4:
-            DDRD  |= (1<<6);
-            PORTD &= ~(1<<6);
-            break;
-        case 5:
-            DDRD  |= (1<<1);
-            PORTD &= ~(1<<1);
-            break;
-        case 6:
-            DDRD  |= (1<<2);
-            PORTD &= ~(1<<2);
-            break;
-        case 7:
-            DDRC  |= (1<<6);
-            PORTC &= ~(1<<6);
-            break;
-        case 8:
-            DDRF  |= (1<<7);
-            PORTF &= ~(1<<7);
-            break;
-    }
+	switch (index) {
+		case 0:
+		case 2:
+		case 4:
+		case 6:
+		case 8:
+		case 13:
+		case 15:
+		case 17:
+			print("unselect port B pin ");
+			phex(pinmap[index]);
+			print("\n");
+			//DDRB CLEAR_PORT(index);
+			//PORTB CLEAR_PORT(index);
+			break;
+		case 18:
+		case 20:
+			print("unselect port C pin ");
+			phex(pinmap[index]);
+			print("\n");
+			//DDRC CLEAR_PORT(index);
+			//PORTC CLEAR_PORT(index);
+			break;
+		case 10:
+		case 12:
+		case 14:
+		case 16:
+		case 19:
+		case 21:
+			print("unselect port D pin ");
+			phex(pinmap[index]);
+			print("\n");
+			//DDRD CLEAR_PORT(index);
+			//PORTD CLEAR_PORT(index);
+			break;
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 9:
+		case 11:
+			print("unselect port F pin ");
+			phex(pinmap[index]);
+			print("\n");
+			//DDRF CLEAR_PORT(index);
+			//PORTF CLEAR_PORT(index);
+			break;
+	}
+}
+
+//inline
+static void unselect(void)
+{
+	for (uint8_t i = 0; i < 22; i++) {
+		if (IS_OUTPUT(portmap[i])) {
+			unselect_pin(i);
+		}
+	}
+}
+
+// Output low(DDR:1, PORT:0) to select
+static void select(uint8_t index)
+{
+	switch (index) {
+		case 0:
+		case 2:
+		case 4:
+		case 6:
+		case 8:
+		case 13:
+		case 15:
+		case 17:
+			print("select port B pin ");
+			phex(pinmap[index]);
+			print("\n");
+			//DDRB SET_PORT(index);
+			//PORTB CLEAR_PORT(index);
+			break;
+		case 18:
+		case 20:
+			print("select port C pin ");
+			phex(pinmap[index]);
+			print("\n");
+			//DDRC SET_PORT(index);
+			//PORTC CLEAR_PORT(index);
+			break;
+		case 10:
+		case 12:
+		case 14:
+		case 16:
+		case 19:
+		case 21:
+			print("select port D pin ");
+			phex(pinmap[index]);
+			print("\n");
+			//DDRD SET_PORT(index);
+			//PORTD CLEAR_PORT(index);
+			break;
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 9:
+		case 11:
+			print("select port F pin ");
+			phex(pinmap[index]);
+			print("\n");
+			//DDRF SET_PORT(index);
+			//PORTF CLEAR_PORT(index);
+			break;
+	}
 }
