b34bb03d69b56d630de36b178651eeb3172f637f
Julien Pecqueur <jpec@julienpecqueur.net>
2015-02-19 11:13:02 +0100

    JPEC40: Update keymap_spacefn.c

--- keyboard/jpec40/keymap_spacefn.c
+++ keyboard/jpec40/keymap_spacefn.c
@@ -5,12 +5,10 @@ This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
 (at your option) any later version.
-
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
-
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -18,7 +16,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "keymap_common.h"
 
 /*
- * JPEC40 KEYMAP SPACE FN ENABLED
+ * JPEC40 KEYMAP SPACE FN
  */
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Keymap 0: Default Layer
