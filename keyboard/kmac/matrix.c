3cc83904dadbdf375aeac50c27e548bb60e1f477
ageaenes <h.viktorin@gmail.com>
2015-08-02 22:39:04 +0200

    Added support for kmac happy, just keymaps and matrix.

--- keyboard/kmac/matrix.c
+++ keyboard/kmac/matrix.c
@@ -28,6 +28,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef DEBOUNCE
 #   define DEBOUNCE	0
 #endif
+
+#ifndef LAYOUT_HAPPY
+#    define LAYOUT_HAPPY 0
+#endif
 static uint8_t debouncing = DEBOUNCE;
 
 /* matrix state(1:on, 0:off) */
@@ -65,15 +69,14 @@ void matrix_init(void)
 
 uint8_t matrix_scan(void)
 {
-    for (uint8_t col = 0; col < MATRIX_COLS; col++) {  // 0-16
-        select_col(col);
+    for (uint8_t col = 0; col < MATRIX_COLS; col++) {
+        select_col(col);    
         _delay_us(3); // TODO: Determine the correct value needed here.
         uint8_t rows = read_rows();
-        // Use the otherwise unused col: 0 row: 3 for caps lock.
         if(col == 0) {
             rows |= read_caps();
         }
-        for (uint8_t row = 0; row < MATRIX_ROWS; row++) {  // 0-5
+        for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
             bool prev_bit = matrix_debouncing[row] & ((matrix_row_t)1<<col);
             bool curr_bit = rows & (1<<row);
             if (prev_bit != curr_bit) {
@@ -96,7 +99,6 @@ uint8_t matrix_scan(void)
             }
         }
     }
-
     return 1;
 }
 
@@ -135,42 +137,58 @@ uint8_t matrix_key_count(void)
     return count;
 }
 
-/* Row pin configuration
+/* Row pin configuration TKL size, On Happy 60%, B7 is not used.
  * row: 0   1   2   3   4   5
  * pin: D0  D1  D2  D3  D5  B7
  *
  * Caps lock uses its own pin PE2
  */
+
 static void init_rows(void)
 {
-    // Input (DDR:0, PORT:0)
+    // Input (DDR:0, PORT:0) for matrix
     DDRD  &= ~0b00101111;
     PORTD &= ~0b00101111;
-    DDRB  &= ~(1<<7);
-    PORTB &= ~(1<<7);
-
-    // Input with pull-up (DDR:0, PORT:1)
+    if(!LAYOUT_HAPPY){
+        DDRB  &= ~(1<<7);
+        PORTB &= ~(1<<7);
+    }
+    // Input with pull-up, for CAPS lock
     DDRE &= ~(1<<2);
     PORTE |= (1<<2);
 }
 
-static uint8_t read_rows(void)
+static uint8_t read_rows()
 {
-    return (PIND&(1<<0) ? (1<<0) : 0) |
-           (PIND&(1<<1) ? (1<<1) : 0) |
-           (PIND&(1<<2) ? (1<<2) : 0) |
-           (PIND&(1<<3) ? (1<<3) : 0) |
-           (PIND&(1<<5) ? (1<<4) : 0) |
-           (PINB&(1<<7) ? (1<<5) : 0);
+    if(LAYOUT_HAPPY){
+        return (PIND&(1<<0) ? (1<<0) : 0) |
+               (PIND&(1<<1) ? (1<<1) : 0) |
+               (PIND&(1<<2) ? (1<<2) : 0) |
+               (PIND&(1<<3) ? (1<<3) : 0) |
+               (PIND&(1<<5) ? (1<<4) : 0);
+    }else{
+        return (PIND&(1<<0) ? (1<<0) : 0) |
+               (PIND&(1<<1) ? (1<<1) : 0) |
+               (PIND&(1<<2) ? (1<<2) : 0) |
+               (PIND&(1<<3) ? (1<<3) : 0) |
+               (PIND&(1<<5) ? (1<<4) : 0) |
+               (PINB&(1<<7) ? (1<<5) : 0);
+    }
+
 }
 
 static uint8_t read_caps(void)
 {
-    return PINE&(1<<2) ? 0 : (1<<3);
+    if(LAYOUT_HAPPY){
+        return PINE&(1<<2) ? 0 : (1<<2);
+    }
+    else{
+        return PINE&(1<<2) ? 0 : (1<<3);
+    }
 }
 
-/* Columns 0 - 15
- * These columns uses two 74HC237D 3 to 8 bit demultiplexers.
+/* Columns 0 - 15 TKL SIZE
+ * These columns uses two 74HC238D 3 to 8 bit demultiplexers.
  * col / pin:    PC6  PB6  PF0  PF1  PC7
  * 0:             1    0    0    0    0
  * 1:             1    0    1    0    0
@@ -192,10 +210,46 @@ static uint8_t read_caps(void)
  * col: 16
  * pin: PB5
  */
+
+ /* Columns 0 - 14 HAPPY SIZE
+ * These columns uses one 74HC237D 3 to 8 bit demultiplexer Column 0-8
+ * col / pin:    PC6  PF0  PF1  PC7  PE6  PB0  PB7  PD4  PD6  PD7  PB4
+ * 0:             1    0    0    0    0    0    0    0    0    0    0
+ * 1:             1    1    0    0    0    0    0    0    0    0    0
+ * 2:             1    0    1    0    0    0    0    0    0    0    0
+ * 3:             1    1    1    0    0    0    0    0    0    0    0
+ * 4:             1    0    0    1    0    0    0    0    0    0    0
+ * 5:             1    1    0    1    0    0    0    0    0    0    0
+ * 6:             1    0    1    1    0    0    0    0    0    0    0
+ * 7:             1    1    1    1    0    0    0    0    0    0    0
+ * 8:             0    0    0    0    1    0    0    0    0    0    0
+ * 9:             0    0    0    0    0    1    0    0    0    0    0
+ * 10:            0    0    0    0    0    0    1    0    0    0    0
+ * 11:            0    0    0    0    0    0    0    1    0    0    0
+ * 12:            0    0    0    0    0    0    0    0    1    0    0
+ * 13:            0    0    0    0    0    0    0    0    0    1    0
+ * 14:            0    0    0    0    0    0    0    0    0    0    1
+ *
+ *
+ *
+ */
 static void unselect_cols(void)
 {
-    DDRB |= (1<<5) | (1<<6);
-    PORTB &= ~((1<<5) | (1<<6));
+    if(LAYOUT_HAPPY){
+
+        DDRB |= (1<<0) | (1<<4) | (1<<7);
+        PORTB &= ~((1<<0) | (1<<4) | (1<<7));
+
+        DDRD |= (1<<4) | (1<<6) | (1<<7);
+        PORTD &= ~((1<<4) | (1<<6) | (1<<7));
+
+        DDRE |= (1<<6);
+        PORTE &= ~(1<<6);
+    }else{
+
+        DDRB |= (1<<5) | (1<<6);
+        PORTB &= ~((1<<5) | (1<<6));
+    }
 
     DDRC |= (1<<6) | (1<<7);
     PORTC &= ~((1<<6) | (1<<7));
@@ -242,42 +296,71 @@ static void select_col(uint8_t col)
             PORTF |= (1<<0) | (1<<1);
             PORTC |= (1<<7);
             break;
-        case 8:
-            PORTB |= (1<<6);
-            break;
-        case 9:
-            PORTB |= (1<<6);
-            PORTF |= (1<<0);
-            break;
-        case 10:
-            PORTB |= (1<<6);
-            PORTF |= (1<<1);
-            break;
-        case 11:
-            PORTB |= (1<<6);
-            PORTF |= (1<<0) | (1<<1);
-            break;
-        case 12:
-            PORTB |= (1<<6);
-            PORTC |= (1<<7);
-            break;
-        case 13:
-            PORTB |= (1<<6);
-            PORTF |= (1<<0);
-            PORTC |= (1<<7);
-            break;
-        case 14:
-            PORTB |= (1<<6);
-            PORTF |= (1<<1);
-            PORTC |= (1<<7);
-            break;
-        case 15:
-            PORTB |= (1<<6);
-            PORTF |= (1<<0) | (1<<1);
-            PORTC |= (1<<7);
-            break;
-        case 16:
-            PORTB |= (1<<5);
-            break;
+    }
+
+    if(LAYOUT_HAPPY){
+        switch(col){
+            case 8:  
+                PORTE |= (1<<6);
+                break;
+            case 9:
+                PORTB |= (1<<0);  
+                break;
+            case 10:
+                PORTB |= (1<<7);
+                break;
+            case 11:
+                PORTD |= (1<<4);
+                break;
+            case 12:
+                PORTD |= (1<<6);
+                break;
+            case 13:
+                PORTD |= (1<<7);
+                break;
+            case 14:
+                PORTB |= (1<<4);
+                break;
+        }
+    }else{
+        switch(col){
+            case 8:
+                PORTB |= (1<<6);
+                break;
+            case 9:
+                PORTB |= (1<<6);
+                PORTF |= (1<<0);
+                break;
+            case 10:
+                PORTB |= (1<<6);
+                PORTF |= (1<<1);
+                break;
+            case 11:
+                PORTB |= (1<<6);
+                PORTF |= (1<<0) | (1<<1);
+                break;
+            case 12:
+                PORTB |= (1<<6);
+                PORTC |= (1<<7);
+                break;
+            case 13:
+                PORTB |= (1<<6);
+                PORTF |= (1<<0);
+                PORTC |= (1<<7);
+                break;
+            case 14:
+                PORTB |= (1<<6);
+                PORTF |= (1<<1);
+                PORTC |= (1<<7);
+                break;
+            case 15:
+                PORTB |= (1<<6);
+                PORTF |= (1<<0) | (1<<1);
+                PORTC |= (1<<7);
+                break;
+            case 16:
+                PORTB |= (1<<5);
+                break;
+        }
     }
 }
