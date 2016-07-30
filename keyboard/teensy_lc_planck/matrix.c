91d31dc392b1dc21c8e97e1213858eed9ed41cef
di0ib <git@porkhash.net>
2016-07-29 21:01:43 -1000

    TV44 and Planck

---
+++ keyboard/teensy_lc_planck/matrix.c
@@ -0,0 +1,203 @@
+/*
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "ch.h"
+#include "hal.h"
+
+/*
+ * scan matrix
+ */
+#include "print.h"
+#include "debug.h"
+#include "util.h"
+#include "matrix.h"
+#include "wait.h"
+
+#ifndef DEBOUNCE
+#   define DEBOUNCE 5
+#endif
+static uint8_t debouncing = DEBOUNCE;
+
+/* matrix state(1:on, 0:off) */
+static matrix_row_t matrix[MATRIX_ROWS];
+static matrix_row_t matrix_debouncing[MATRIX_ROWS];
+
+static matrix_row_t read_cols(void);
+static void init_cols(void);
+static void unselect_rows(void);
+static void select_row(uint8_t row);
+
+
+inline
+uint8_t matrix_rows(void)
+{
+    return MATRIX_ROWS;
+}
+
+inline
+uint8_t matrix_cols(void)
+{
+    return MATRIX_COLS;
+}
+
+#define LED_ON()    do { palSetPad(TEENSY_PIN13_IOPORT, TEENSY_PIN13) ;} while (0)
+#define LED_OFF()   do { palClearPad(TEENSY_PIN13_IOPORT, TEENSY_PIN13); } while (0)
+#define LED_TGL()   do { palTogglePad(TEENSY_PIN13_IOPORT, TEENSY_PIN13); } while (0)
+
+void matrix_init(void)
+{
+    // initialize row and col
+    unselect_rows();
+    init_cols();
+
+    // initialize matrix state: all keys off
+    for (uint8_t i=0; i < MATRIX_ROWS; i++) {
+        matrix[i] = 0;
+        matrix_debouncing[i] = 0;
+    }
+
+    //debug
+    debug_matrix = true;
+    LED_ON();
+    wait_ms(500);
+    LED_OFF();
+}
+
+uint8_t matrix_scan(void)
+{
+    for (uint8_t i = 0; i < MATRIX_ROWS; i++) {
+        select_row(i);
+        //wait_us(30);  // without this wait read unstable value.
+		for (uint8_t i=0; i < 1; i++) { // Loop for very short delay, wait_us is returning a 1ms delay
+			LED_TGL(); // toggle the onboard LED attached to pin 13
+		}
+        matrix_row_t cols = read_cols();
+        if (matrix_debouncing[i] != cols) {
+            matrix_debouncing[i] = cols;
+            if (debouncing) {
+                debug("bounce!: "); debug_hex(debouncing); debug("\n");
+            }
+            debouncing = DEBOUNCE;
+        }
+        unselect_rows();
+    }
+
+    if (debouncing) {
+        if (--debouncing) {
+            wait_ms(1);
+        } else {
+            for (uint8_t i = 0; i < MATRIX_ROWS; i++) {
+                matrix[i] = matrix_debouncing[i];
+            }
+        }
+    }
+
+    return 1;
+}
+
+inline
+bool matrix_is_on(uint8_t row, uint8_t col)
+{
+    return (matrix[row] & ((matrix_row_t)1<<col));
+}
+
+inline
+matrix_row_t matrix_get_row(uint8_t row)
+{
+    return matrix[row];
+}
+
+void matrix_print(void)
+{
+    print("\nr/c 0123456789ABCDEF\n");
+    for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
+        phex(row); print(": ");
+        pbin_reverse16(matrix_get_row(row));
+        print("\n");
+    }
+}
+
+/* Column pin configuration
+ */
+static void  init_cols(void)
+{
+    // internal pull-up
+    palSetPadMode(TEENSY_PIN10_IOPORT, TEENSY_PIN10, PAL_MODE_INPUT_PULLUP);
+    palSetPadMode(TEENSY_PIN11_IOPORT, TEENSY_PIN11, PAL_MODE_INPUT_PULLUP);
+    palSetPadMode(TEENSY_PIN12_IOPORT, TEENSY_PIN12, PAL_MODE_INPUT_PULLUP);
+    palSetPadMode(TEENSY_PIN14_IOPORT, TEENSY_PIN14, PAL_MODE_INPUT_PULLUP);
+    palSetPadMode(TEENSY_PIN15_IOPORT, TEENSY_PIN15, PAL_MODE_INPUT_PULLUP);
+    palSetPadMode(TEENSY_PIN16_IOPORT, TEENSY_PIN16, PAL_MODE_INPUT_PULLUP);
+    palSetPadMode(TEENSY_PIN18_IOPORT, TEENSY_PIN18, PAL_MODE_INPUT_PULLUP);
+    palSetPadMode(TEENSY_PIN19_IOPORT, TEENSY_PIN19, PAL_MODE_INPUT_PULLUP);
+    palSetPadMode(TEENSY_PIN20_IOPORT, TEENSY_PIN20, PAL_MODE_INPUT_PULLUP);
+    palSetPadMode(TEENSY_PIN21_IOPORT, TEENSY_PIN21, PAL_MODE_INPUT_PULLUP);
+    palSetPadMode(TEENSY_PIN22_IOPORT, TEENSY_PIN22, PAL_MODE_INPUT_PULLUP);
+    palSetPadMode(TEENSY_PIN23_IOPORT, TEENSY_PIN23, PAL_MODE_INPUT_PULLUP);
+}
+
+/* Returns status of switches(1:on, 0:off) */
+static matrix_row_t read_cols(void)
+{
+    return ((palReadPad(TEENSY_PIN10_IOPORT, TEENSY_PIN10)==PAL_HIGH) ? 0 : (1<<0))
+         | ((palReadPad(TEENSY_PIN11_IOPORT, TEENSY_PIN11)==PAL_HIGH) ? 0 : (1<<1))
+         | ((palReadPad(TEENSY_PIN12_IOPORT, TEENSY_PIN12)==PAL_HIGH) ? 0 : (1<<2))
+         | ((palReadPad(TEENSY_PIN14_IOPORT, TEENSY_PIN14)==PAL_HIGH) ? 0 : (1<<3))
+         | ((palReadPad(TEENSY_PIN15_IOPORT, TEENSY_PIN15)==PAL_HIGH) ? 0 : (1<<4))
+         | ((palReadPad(TEENSY_PIN16_IOPORT, TEENSY_PIN16)==PAL_HIGH) ? 0 : (1<<5))
+         | ((palReadPad(TEENSY_PIN18_IOPORT, TEENSY_PIN18)==PAL_HIGH) ? 0 : (1<<6))
+         | ((palReadPad(TEENSY_PIN19_IOPORT, TEENSY_PIN19)==PAL_HIGH) ? 0 : (1<<7))
+         | ((palReadPad(TEENSY_PIN20_IOPORT, TEENSY_PIN20)==PAL_HIGH) ? 0 : (1<<8))
+         | ((palReadPad(TEENSY_PIN21_IOPORT, TEENSY_PIN21)==PAL_HIGH) ? 0 : (1<<9))
+         | ((palReadPad(TEENSY_PIN22_IOPORT, TEENSY_PIN22)==PAL_HIGH) ? 0 : (1<<10))
+         | ((palReadPad(TEENSY_PIN23_IOPORT, TEENSY_PIN23)==PAL_HIGH) ? 0 : (1<<11))
+         | ((palReadPad(TEENSY_PIN23_IOPORT, TEENSY_PIN23)==PAL_HIGH) ? 0 : (1<<11));
+}
+
+/* Row pin configuration
+ */
+static void unselect_rows(void)
+{
+    palSetPadMode(TEENSY_PIN6_IOPORT, TEENSY_PIN6, PAL_MODE_INPUT); // hi-Z
+    palSetPadMode(TEENSY_PIN7_IOPORT, TEENSY_PIN7, PAL_MODE_INPUT); // hi-Z
+    palSetPadMode(TEENSY_PIN8_IOPORT, TEENSY_PIN8, PAL_MODE_INPUT); // hi-Z
+    palSetPadMode(TEENSY_PIN9_IOPORT, TEENSY_PIN9, PAL_MODE_INPUT); // hi-Z
+}
+
+static void select_row(uint8_t row)
+{
+    (void)row;
+    // Output low to select
+    switch (row) {
+        case 0:
+            palSetPadMode(TEENSY_PIN6_IOPORT, TEENSY_PIN6, PAL_MODE_OUTPUT_PUSHPULL);
+            palClearPad(TEENSY_PIN6_IOPORT, TEENSY_PIN6);
+            break;
+        case 1:
+            palSetPadMode(TEENSY_PIN7_IOPORT, TEENSY_PIN7, PAL_MODE_OUTPUT_PUSHPULL);
+            palClearPad(TEENSY_PIN7_IOPORT, TEENSY_PIN7);
+            break;
+        case 2:
+            palSetPadMode(TEENSY_PIN8_IOPORT, TEENSY_PIN8, PAL_MODE_OUTPUT_PUSHPULL);
+            palClearPad(TEENSY_PIN8_IOPORT, TEENSY_PIN8);
+            break;
+        case 3:
+            palSetPadMode(TEENSY_PIN9_IOPORT, TEENSY_PIN9, PAL_MODE_OUTPUT_PUSHPULL);
+            palClearPad(TEENSY_PIN9_IOPORT, TEENSY_PIN9);
+            break;
+    }
+}
---
+++ keyboard/teensy_lc_tv44/keymap_tv44.c
@@ -0,0 +1,53 @@
+#include "keymap_common.h"
+
+const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* 0: qerty */
+    KEYMAP(ESC,  Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   BSPC, \
+           FN0,   A,   S,   D,   F,   G,   H,   J,   K,   L,  SCLN,QUOT, \
+           LSFT,   Z,   X,   C,   V,   B,   N,   M,  COMM,DOT,SLSH,FN20, \
+           LCTL,  LGUI,  LALT,     FN1,      FN2,      RALT,  APP, RCTL),
+
+    /* 1: FN 1 */
+    KEYMAP(FN3,   FN4, FN5, FN6, FN7, FN8, FN9,FN10,FN11,FN12,FN13, DEL, \
+           TRNS, TRNS,TRNS,PAUS,TRNS,TRNS,TRNS,FN16,FN17,FN18,FN19,FN14, \
+           TRNS, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9, F10,  TRNS, \
+           TRNS, TRNS,  TRNS,      TRNS,    TRNS,      MNXT, MUTE, MPLY),
+
+    /* 2: FN 2 */
+    KEYMAP(GRV,     1,   2,   3,   4,   5,   6,   7,  8,   9,   0,  DEL, \
+           TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,MINS,EQL,LBRC,RBRC, BSLS, \
+           TRNS, F11,  F12, F13, F14, F15, F16, F17, F18,PGUP,F20, TRNS, \
+           TRNS, TRNS,  TRNS,      TRNS,    TRNS,      HOME, PGDN, END),		   
+		   
+    /* 3: TAB */
+    KEYMAP(TRNS, CALC,WHOM,MAIL,TRNS,MYCM,TRNS,TRNS,TRNS,TRNS,PSCR, DEL, \
+           TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+           TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,UP,  TRNS,TRNS, \
+           TRNS, TRNS,  TRNS,      TRNS,    TRNS,      LEFT, DOWN, RGHT),
+};
+
+const uint16_t PROGMEM fn_actions[] = {
+    [0]  = ACTION_LAYER_TAP_KEY(3, KC_TAB),
+    [1]  = ACTION_LAYER_TAP_KEY(1, KC_SPC),
+    [2]  = ACTION_LAYER_TAP_KEY(2, KC_SPC),
+    [3]  = ACTION_MODS_KEY(MOD_LSFT, KC_GRV),
+    [4]  = ACTION_MODS_KEY(MOD_LSFT, KC_1),
+    [5]  = ACTION_MODS_KEY(MOD_LSFT, KC_2),
+    [6]  = ACTION_MODS_KEY(MOD_LSFT, KC_3),
+    [7]  = ACTION_MODS_KEY(MOD_LSFT, KC_4),
+    [8]  = ACTION_MODS_KEY(MOD_LSFT, KC_5),
+    [9]  = ACTION_MODS_KEY(MOD_LSFT, KC_6),
+    [10] = ACTION_MODS_KEY(MOD_LSFT, KC_7),
+    [11] = ACTION_MODS_KEY(MOD_LSFT, KC_8),
+    [12] = ACTION_MODS_KEY(MOD_LSFT, KC_9),
+    [13] = ACTION_MODS_KEY(MOD_LSFT, KC_0),
+    [14] = ACTION_MODS_KEY(MOD_LSFT, KC_BSLS),
+    [15] = ACTION_MODS_KEY(MOD_LSFT, KC_QUOT),
+    [16] = ACTION_MODS_KEY(MOD_LSFT, KC_MINS),
+    [17] = ACTION_MODS_KEY(MOD_LSFT, KC_EQL),
+    [18] = ACTION_MODS_KEY(MOD_LSFT, KC_LBRC),
+    [19] = ACTION_MODS_KEY(MOD_LSFT, KC_RBRC),
+	[20] = ACTION_MODS_TAP_KEY(MOD_RSFT, KC_ENT),
+};
+
+
