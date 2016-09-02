e4a04dba95ec5e7a344ead9d7487e5d91e8fe6d1
Jun Wako <wakojun@gmail.com>
2016-09-02 13:54:57 +0900

    Fix config.js

--- editor/unimap/config.js
+++ editor/unimap/config.js
@@ -5,13 +5,16 @@ keymap_config = {
     hhkb: {
         firmware_url: "https://raw.githubusercontent.com/tmk/tmk_keyboard/master/keyboard/hhkb/binary/hhkb_unimap.hex"
     },
+    adb_usb: {
+        firmware_url: "https://raw.githubusercontent.com/tmk/tmk_keyboard/master/converter/adb_usb/binary/adb_usb_rev2_unimap.hex",
+    },
     adb_usb_rev2: {
         firmware_url: "https://raw.githubusercontent.com/tmk/tmk_keyboard/master/converter/adb_usb/binary/adb_usb_rev2_unimap.hex",
     },
     adb_usb_rev1: {
         firmware_url: "https://raw.githubusercontent.com/tmk/tmk_keyboard/master/converter/adb_usb/binary/adb_usb_rev1_unimap.hex",
-    }
+    },
     usb_usb: {
         firmware_url: "https://raw.githubusercontent.com/tmk/tmk_keyboard/master/converter/usb_usb/binary/usb_usb_unimap.hex",
-    }
+    },
 }
