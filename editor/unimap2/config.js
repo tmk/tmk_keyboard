c869f85867530d21b01a8120281f5c5e9a2a430f
Jun Wako <wakojun@gmail.com>
2016-09-09 11:42:08 +0900

    Add keyboard layout load on unimap

---
+++ editor/unimap2/config.js
@@ -0,0 +1,33 @@
+keymap_config = {
+    hhkb: {
+        desc: "HHKB Pro1/Pro2 Controller",
+        firmware_url: "https://raw.githubusercontent.com/tmk/tmk_keyboard/master/keyboard/hhkb/binary/hhkb_unimap.hex",
+        firmware_format: "ihex",
+        layout: "layout-hhkb.html",
+    },
+    hhkb_jp: {
+        desc: "HHKB JP Controller",
+        firmware_url: "https://raw.githubusercontent.com/tmk/tmk_keyboard/master/keyboard/hhkb/binary/hhkb_jp_unimap.hex",
+        firmware_format: "ihex",
+        layout: "layout-hhkb_jp.html",
+    },
+    adb_usb: {
+        desc: "ADB-USB Converter(new; ATMega32u2)",
+        id: "adb_usb",
+        firmware_url: "https://raw.githubusercontent.com/tmk/tmk_keyboard/master/converter/adb_usb/binary/adb_usb_rev2_unimap.hex",
+        firmware_format: "ihex",
+        //layout: "layout-adb_usb.html",
+    },
+    adb_usb_rev1: {
+        desc: "ADB-USB Converter rev.1(old; ATMega32u4)",
+        firmware_url: "https://raw.githubusercontent.com/tmk/tmk_keyboard/master/converter/adb_usb/binary/adb_usb_rev1_unimap.hex",
+        firmware_format: "ihex",
+        //layout: "layout-adb_usb.html",
+    },
+    usb_usb: {
+        desc: "USB-USB Converter",
+        firmware_url: "https://raw.githubusercontent.com/tmk/tmk_keyboard/master/converter/usb_usb/binary/usb_usb_unimap.hex",
+        firmware_format: "ihex",
+        layout: "layout-128key.html",
+    },
+}
