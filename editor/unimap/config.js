9ad2cdab68234a92c35b77bd31fc3136dd3ea2e5
Jun Wako <wakojun@gmail.com>
2016-09-12 12:27:12 +0900

    Add adb_usb layout

--- editor/unimap/config.js
+++ editor/unimap/config.js
@@ -30,13 +30,13 @@ CONFIG = {
             id: "adb_usb",
             firmware_url: "https://raw.githubusercontent.com/tmk/tmk_keyboard/master/converter/adb_usb/binary/adb_usb_rev2_unimap.hex",
             firmware_format: "ihex",
-            //layout: "layout-adb_usb.html",
+            layout: "layout-adb_usb.html",
         },
         adb_usb_rev1: {
             desc: "ADB-USB Converter rev.1(old; ATMega32u4)",
             firmware_url: "https://raw.githubusercontent.com/tmk/tmk_keyboard/master/converter/adb_usb/binary/adb_usb_rev1_unimap.hex",
             firmware_format: "ihex",
-            //layout: "layout-adb_usb.html",
+            layout: "layout-adb_usb.html",
         },
         usb_usb: {
             desc: "USB-USB Converter",
