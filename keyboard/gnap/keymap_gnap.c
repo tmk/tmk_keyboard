5425f57f573536d3b6de8b35fdabba46d0a6af50
di0ib <git@porkhash.net>
2016-09-18 10:51:29 -1000

    update keymap

--- keyboard/gnap/keymap_gnap.c
+++ keyboard/gnap/keymap_gnap.c
@@ -18,7 +18,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 KEYMAP(
   KC_ESC,   KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,    KC_Y,     KC_U,      KC_I,      KC_O,       KC_P,       KC_BSPC,
   KC_FN2,   KC_A,    KC_S,    KC_D,    KC_F,     KC_G,    KC_H,     KC_J,      KC_K,      KC_L,       KC_SCLN,    KC_QUOT,
-  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,    KC_N,     KC_M,      KC_COMM,   KC_DOT,     KC_SLSH,    KC_SLSH,
+  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,    KC_N,     KC_M,      KC_COMM,   KC_DOT,     KC_SLSH,    KC_FN16,
   KC_LCTL,  KC_LALT, KC_LGUI, KC_APP,  KC_FN1,   KC_SPC,            KC_FN0,    KC_LEFT,   KC_DOWN,    KC_UP,      KC_RGHT
 ),
 
@@ -66,6 +66,7 @@ const action_t PROGMEM fn_actions[] = {
     [13] = ACTION_MODS_KEY(MOD_LSFT, KC_8),
     [14] = ACTION_MODS_KEY(MOD_LSFT, KC_9),
     [15] = ACTION_MODS_KEY(MOD_LSFT, KC_0),
+    [16] = ACTION_MODS_TAP_KEY(MOD_RSFT, KC_ENT),
 };
 
 void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
