3b816b959343db16845b48530770551ef0a2b526
di0ib <git@porkhash.net>
2016-10-19 19:34:17 -1000

    pro micro LEDs as capslock indicator

--- keyboard/kwark/led.c
+++ keyboard/kwark/led.c
@@ -22,5 +22,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 void led_set(uint8_t usb_led)
 {
-
+    if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
+        // output low
+        DDRB |= (1<<0);
+        PORTB &= ~(1<<0);
+        DDRD |= (1<<5);
+        PORTD &= ~(1<<5);
+    } else {
+        // Hi-Z
+        DDRB &= ~(1<<0);
+        PORTB &= ~(1<<0);
+        DDRD &= ~(1<<5);
+        PORTD &= ~(1<<5);
+    }
 }
