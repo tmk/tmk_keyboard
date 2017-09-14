4dc2fb5b312e37f26670011708653296df100d8c
tmk <hasu@tmk-kbd.com>
2017-09-14 12:59:54 +0900

    onekey: Fix for V-USB of ATtiny85 and key scan

--- keyboard/onekey/usbconfig.h
+++ keyboard/onekey/usbconfig.h
@@ -24,11 +24,11 @@ section at the end of this file).
 
 /* ---------------------------- Hardware Config ---------------------------- */
 
-#define USB_CFG_IOPORTNAME      D
+#define USB_CFG_IOPORTNAME      B
 /* This is the port where the USB bus is connected. When you configure it to
  * "B", the registers PORTB, PINB and DDRB will be used.
  */
-#define USB_CFG_DMINUS_BIT      3
+#define USB_CFG_DMINUS_BIT      1
 /* This is the bit number in USB_CFG_IOPORT where the USB D- line is connected.
  * This may be any bit in the port.
  */
