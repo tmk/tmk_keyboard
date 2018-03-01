eecfb98bf27682ea1b9b9ede40b2d5da4fd13fda
di0ib <di0ib@users.noreply.github.com>
2018-03-01 08:22:42 -1000

    5x5

---
+++ keyboard/5x5/keymap_5x5.c
@@ -0,0 +1,27 @@
+#include "keymap_common.h"
+
+const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+KEYMAP(
+    ESC,  1,    2,    3,    4,    5,    6,    7,    8,    9,    0,    MINS, EQL,  BSPC, BSPC,
+    TAB,  Q,    W,    E,    R,    T,    Y,    U,    I,    O,    P,    LBRC, RBRC, BSLS, GRV,
+    CAPS, A,    S,    D,    F,    G,    H,    J,    K,    L,    SCLN, QUOT, ENT,  ENT,  PGUP,
+    LSFT, Z,    X,    C,    V,    B,    N,    M,    COMM, DOT,  SLSH, RSFT, RSFT, UP,   PGDN,
+    LCTL, LGUI, LALT, APP,  SPC,  SPC,  SPC,  SPC,  FN0,  RALT, RGUI, RCTL, LEFT, DOWN, RGHT
+),
+KEYMAP(
+    TRNS, F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,   F10,  F11,  F12,  BSPC, DEL,
+    TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,
+    TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,
+    TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,
+    TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS
+),
+};
+
+const action_t PROGMEM fn_actions[] = {
+    [0]  = ACTION_LAYER_MOMENTARY(1),
+};
+
+void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
+
+}
