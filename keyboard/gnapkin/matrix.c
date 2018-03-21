b647b2deef3086e4f4171804d68773d8886025c0
di0ib <di0ib@users.noreply.github.com>
2018-03-21 07:52:44 -1000

    Update matrix.c

--- keyboard/gnapkin/matrix.c
+++ keyboard/gnapkin/matrix.c
@@ -143,8 +143,8 @@ uint8_t matrix_key_count(void)
 
 /* Column pin configuration
  * col: 0   1   2   3   4   5   6   7   8   9   10  11 
- * pin: D7  E6  B4  B5  B6  B2  B3  B1  F7  F6  F5  F4
-        C6, D7, E6, B4, B6, B2, B3, B1, F7, F6, F5, F4
+ * pin: C6  D7  E6  B4  B6  B2  B3  B1  F7  F6  F5  F4
+        
  */
  
 static void  init_cols(void)
