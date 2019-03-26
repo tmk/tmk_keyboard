4fb5e66a3b12ae0ce0b5d4ca9c45ef7dd589eecd
Le Zhang <you@example.com>
2019-03-26 10:20:31 -0400

    map eject and fn to F23 and F24

--- tmk_core/protocol/usb_hid/parser.cpp
+++ tmk_core/protocol/usb_hid/parser.cpp
@@ -20,10 +20,13 @@ void KBDReportParser::Parse(USBHID *hid, bool is_rpt_id, uint8_t len, uint8_t *b
     // 0x02: fn key (bottom left key) pressed
     // 0x03: fn and eject key pressed at the same time
     // we store buf[9] in buf[1] (reserved byte)
+    // and map eject and fn to F23 and F24
     if (buf[0] == 0x01 && len == 10) {
         uint8_t t = buf[0];
         buf[0] = buf[1];
         buf[1] = buf[9];
+        if (buf[9] & 0x01) buf[6] = KC_F23; // eject -> F23
+        if (buf[9] & 0x02) buf[7] = KC_F24; // Fn -> F24
     } else if (buf[1] == 0x01) {
         // ignore Cherry 0101010101010101 bug report
         // https://geekhack.org/index.php?topic=69169.msg2638223#msg2638223
