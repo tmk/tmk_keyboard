2fd396b8ca15592729f2ab84fce474bf987deb11
tmk <hasu@tmk-kbd.com>
2019-05-08 12:53:15 +0900

    usb_usb: Add option for disabling suspend power down

--- converter/usb_usb/config.h
+++ converter/usb_usb/config.h
@@ -36,4 +36,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* key combination for command */
 #define IS_COMMAND() (keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT))) 
 
+// Disable power saving in USB suspend loop but remote wakeup is still valid.
+// This allows keep USB::Task() going during suspend without power down time delay.
+//#define NO_SUSPEND_POWER_DOWN
+
 #endif
