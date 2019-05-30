58cbbb5c083ab8209f1ff85ace9d1790399abeb9
tmk <hasu@tmk-kbd.com>
2019-05-30 11:06:13 +0900

    alps64: Change keymap for PCB check

--- keyboard/alps64/unimap_plain.c
+++ keyboard/alps64/unimap_plain.c
@@ -17,9 +17,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "unimap_trans.h"
 
 
-#define AC_L1       ACTION_LAYER_TAP_KEY(1, KC_RGUI)
-
-
 #ifdef KEYMAP_SECTION_ENABLE
 const action_t actionmaps[][UNIMAP_ROWS][UNIMAP_COLS] __attribute__ ((section (".keymap.keymaps"))) = {
 #else
@@ -41,14 +38,7 @@ const action_t actionmaps[][UNIMAP_ROWS][UNIMAP_COLS] PROGMEM = {
     [0] = UNIMAP_ALPS64( \
         GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, NUHS,BSPC, \
         TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,     BSLS, \
-        LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,          ENT,  \
+        CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,          ENT,  \
         LSFT,NUBS,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,     RSFT,ESC,  \
-        LCTL,LGUI,LALT,          SPC,                          APP, RALT,L1,  RCTL),
-
-    [1] = UNIMAP_ALPS64( \
-        ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, DEL,  \
-        CAPS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PSCR,SLCK,PAUS, UP, TRNS,     BSPC, \
-        TRNS,VOLD,VOLU,MUTE,TRNS,TRNS,PAST,PSLS,HOME,PGUP,LEFT,RGHT,          PENT, \
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PPLS,PMNS,END, PGDN,DOWN,     TRNS,TRNS, \
-        TRNS,TRNS,TRNS,          TRNS,                         TRNS,TRNS,TRNS,TRNS),
+        LCTL,LALT,LGUI,          SPC,                          RGUI,RALT,APP, RCTL),
 };
