ae183105bba6a1c22345dd74619747d02900d29b
tmk <hasu@tmk-kbd.com>
2022-04-04 12:58:23 +0900

    adb_usb: Disable 16-bit extended USB mouse report
    16-bit extended report is not required in most devices.
    Turbo Mouse 5 even uses range limited -126 to 126 for XY

--- converter/adb_usb/config.h
+++ converter/adb_usb/config.h
@@ -40,7 +40,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define LOCKING_RESYNC_ENABLE
 
 // Mouse Extended Report
-#define MOUSE_EXT_REPORT
+//#define MOUSE_EXT_REPORT
 
 // Second button in Apple Classic Mouse protocol is ignored as Mac OS9 and OSX does.
 // Without this some of 2-button mouses are unusable.
