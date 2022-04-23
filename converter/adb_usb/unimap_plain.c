2a92acdd2b6659b2f1599fa0c103264897ac7a5d
tmk <hasu@tmk-kbd.com>
2022-04-23 15:47:57 +0900

    adb_usb: Add support for JIS keys

--- converter/adb_usb/unimap_plain.c
+++ converter/adb_usb/unimap_plain.c
@@ -31,27 +31,24 @@ const action_t actionmaps[][UNIMAP_ROWS][UNIMAP_COLS] PROGMEM = {
      * |Esc|   |F1 |F2 |F3 |F4 | |F5 |F6 |F7 |F8 | |F9 |F10|F11|F12| |PrS|ScL|Pau|             |Pwr|
      * `---'   `---------------' `---------------' `---------------' `-----------'             `---'
      * ,-----------------------------------------------------------. ,-----------. ,---------------. ,---.
-     * |  `|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backspa| |Ins|Hom|PgU| |NmL|  =|  /|  *| |VUp|
+     * |  `|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|JPY| BS| |Ins|Hom|PgU| |NmL|  =|  /|  *| |VUp|
      * |-----------------------------------------------------------| |-----------| |---------------| |---|
-     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    \| |Del|End|PgD| |  7|  8|  9|  -| |VDn|
+     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  | |Del|End|PgD| |  7|  8|  9|  -| |VDn|
      * |-----------------------------------------------------------| `-----------' |---------------| |---|
-     * |CapsLo|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|#  |Retn|               |  4|  5|  6|  +| |Mut|
+     * |CapsLo|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|  #| Ret|               |  4|  5|  6|  +| |Mut|
      * |-----------------------------------------------------------|     ,---.     |---------------| `---'
-     * |Shif|\  |  Z|  X|  C|  V|  B|  N|  M|  ,|  ,|  /|Shift     |     |Up |     |  1|  2|  3|   |
+     * |Shif|  <|  Z|  X|  C|  V|  B|  N|  M|  ,|  ,|  /| RO| Shift|     |Up |     |  1|  2|  3|   |
      * |-----------------------------------------------------------| ,-----------. |-----------|Ent| ,---.
-     * |Ctrl |Opt |Gui |         Space           |Gui* |Opt |Ctrl  | |Lef|Dow|Rig| |      0|  .|   | |Ply|
+     * |Ctrl |Opt |Gui  |MHN|    Space    |HNK|Ent|Gui* |Opt |Ctrl | |Lef|Dow|Rig| |  0|  ,|  .|   | |Ply|
      * `-----------------------------------------------------------' `-----------' `---------------' `---'
-     * NOTE: Not-extended ADB keyboards have no discrimination between left and right modifiers.
-     * Use left ones for mapping. Right modifier always sends same code as left one.
-     * Apple Extended Keyboard can discriminate the modifiers except for Command(GUI) key.
      */
     [0] = UNIMAP_ADB(
     ESC,      F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12,        PSCR,SLCK,PAUS,                 PKEY,
-    FN0, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL,      BSPC,  INS, HOME,PGUP,  NLCK,EQL, PSLS,PAST,  VOLU,
+    FN0, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, JPY, BSPC,  INS, HOME,PGUP,  NLCK,EQL, PSLS,PAST,  VOLU,
     TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,     FN1,   DEL, END, PGDN,  P7,  P8,  P9,  PMNS,  VOLD,
     LCAP,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     NUHS,ENT,                    P4,  P5,  P6,  PPLS,  MUTE,
-    LSFT,NUBS,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,          RSFT,       UP,         P1,  P2,  P3,  PENT,
-    LCTL,LALT,LGUI,               SPC,                               RALT,RCTL,  LEFT,DOWN,RGHT,  P0,       PDOT,       MPLY
+    LSFT,NUBS,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,     RO,  RSFT,       UP,         P1,  P2,  P3,  PENT,
+    LCTL,LALT,LGUI,MHEN,          SPC,                HENK,KANA,     RALT,RCTL,  LEFT,DOWN,RGHT,  P0,  PCMM,PDOT,       MPLY
     ),
 
     /*
@@ -59,24 +56,24 @@ const action_t actionmaps[][UNIMAP_ROWS][UNIMAP_COLS] PROGMEM = {
      * |  `|   |   |   |   |   | |   |   |   |   | |   |   |   |   | |   |   |   |             |   |
      * `---'   `---------------' `---------------' `---------------' `-----------'             `---'
      * ,-----------------------------------------------------------. ,-----------. ,---------------. ,---.
-     * |Esc| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Delete | |   |   |   | |   |   |   |   | |   |
+     * |Esc| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Ins|Del| |   |   |   | |   |   |   |   | |   |
      * |-----------------------------------------------------------| |-----------| |---------------| |---|
      * |     |   |   |   |   |   |   |   |PrS|ScL|Pau|Up |INS|     | |   |   |   | |   |   |   |   | |   |
      * |-----------------------------------------------------------| `-----------' |---------------| |---|
      * |CapsLo|VoD|VoU|Mut|   |   |   |   |Hom|PgU|Lef|Rig|#  |Retn|               |   |   |   |   | |   |
      * |-----------------------------------------------------------|     ,---.     |---------------| `---'
-     * |    |   |   |   |   |   |   |   |   |End|PgD|Dow|Shift     |     |PgU|     |   |   |   |   |
+     * |    |   |   |   |   |   |   |   |   |End|PgD|Dow| RO| Shift|     |PgU|     |   |   |   |   |
      * |-----------------------------------------------------------| ,-----------. |-----------|   | ,---.
-     * |     |    |    |                         |     |    |      | |Hom|PgD|End| |       |   |   | |   |
+     * |     |    |    |    |                 |   |     |    |     | |Hom|PgD|End| |   |   |   |   | |   |
      * `-----------------------------------------------------------' `-----------' `---------------' `---'
      */
     [1] = UNIMAP_ADB(
     GRV,      TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,       TRNS,TRNS,TRNS,                 TRNS,
-    ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12,      DEL,   TRNS,TRNS,TRNS,  TRNS,TRNS,TRNS,TRNS,  TRNS,
+    ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, DEL,   TRNS,TRNS,TRNS,  TRNS,TRNS,TRNS,TRNS,  TRNS,
     TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PSCR,SLCK,PAUS,UP,  INS,      TRNS,  TRNS,TRNS,TRNS,  TRNS,TRNS,TRNS,TRNS,  TRNS,
     TRNS,VOLD,VOLU,MUTE,TRNS,TRNS,TRNS,TRNS,HOME,PGUP,LEFT,RGHT,     TRNS,TRNS,                   TRNS,TRNS,TRNS,TRNS,  TRNS,
-    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,END, PGDN,DOWN,          TRNS,       PGUP,       TRNS,TRNS,TRNS,TRNS,
-    TRNS,TRNS,TRNS,               TRNS,                              TRNS,TRNS,  HOME,PGDN,END,   TRNS,     TRNS,       TRNS
+    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,END, PGDN,DOWN,     TRNS,TRNS,       PGUP,       TRNS,TRNS,TRNS,TRNS,
+    TRNS,TRNS,TRNS,TRNS,          TRNS,               TRNS,TRNS,     TRNS,TRNS,  HOME,PGDN,END,   TRNS,TRNS,TRNS,       TRNS
     ),
 
     /*
@@ -84,23 +81,23 @@ const action_t actionmaps[][UNIMAP_ROWS][UNIMAP_COLS] PROGMEM = {
      * |  `|   |   |   |   |   | |   |   |   |   | |   |   |   |   | |   |   |   |             |   |
      * `---'   `---------------' `---------------' `---------------' `-----------'             `---'
      * ,-----------------------------------------------------------. ,-----------. ,---------------. ,---.
-     * |Esc| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Delete | |   |   |   | |   |   |   |   | |   |
+     * |Esc| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Ins|Del| |   |   |   | |   |   |   |   | |   |
      * |-----------------------------------------------------------| |-----------| |---------------| |---|
      * |     |   |   |   |   |   |   |   |PrS|ScL|Pau|Up |INS|     | |   |   |   | |   |   |   |   | |   |
      * |-----------------------------------------------------------| `-----------' |---------------| |---|
      * |CapsLo|VoD|VoU|Mut|   |   |   |   |Hom|PgU|Lef|Rig|#  |Retn|               |   |   |   |   | |   |
      * |-----------------------------------------------------------|     ,---.     |---------------| `---'
-     * |    |   |   |   |   |   |   |   |   |End|PgD|Dow|Shift     |     |PgU|     |   |   |   |   |
+     * |    |   |   |   |   |   |   |   |   |End|PgD|Dow| RO| Shift|     |PgU|     |   |   |   |   |
      * |-----------------------------------------------------------| ,-----------. |-----------|   | ,---.
-     * |     |    |    |                         |     |    |      | |Hom|PgD|End| |       |   |   | |   |
+     * |     |    |    |    |                 |   |     |    |     | |Hom|PgD|End| |   |   |   |   | |   |
      * `-----------------------------------------------------------' `-----------' `---------------' `---'
      */
     [2] = UNIMAP_ADB(
     GRV,      TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,       TRNS,TRNS,TRNS,                 TRNS,
-    ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12,      DEL,   TRNS,TRNS,TRNS,  TRNS,TRNS,TRNS,TRNS,  TRNS,
+    ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, DEL,   TRNS,TRNS,TRNS,  TRNS,TRNS,TRNS,TRNS,  TRNS,
     TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PSCR,SLCK,PAUS,UP,  INS,      TRNS,  TRNS,TRNS,TRNS,  TRNS,TRNS,TRNS,TRNS,  TRNS,
     TRNS,VOLD,VOLU,MUTE,TRNS,TRNS,TRNS,TRNS,HOME,PGUP,LEFT,RGHT,     TRNS,TRNS,                   TRNS,TRNS,TRNS,TRNS,  TRNS,
-    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,END, PGDN,DOWN,          TRNS,       PGUP,       TRNS,TRNS,TRNS,TRNS,
-    TRNS,TRNS,TRNS,               TRNS,                              TRNS,TRNS,  HOME,PGDN,END,   TRNS,     TRNS,       TRNS
+    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,END, PGDN,DOWN,     TRNS,TRNS,       PGUP,       TRNS,TRNS,TRNS,TRNS,
+    TRNS,TRNS,TRNS,TRNS,          TRNS,                    TRNS,     TRNS,TRNS,  HOME,PGDN,END,   TRNS,TRNS,TRNS,       TRNS
     ),
 };
