46a060cd88c0a9dd00ce462eb47200dcfb744db6
tmk <hasu@tmk-kbd.com>
2023-03-22 23:11:43 +0900

    core: Include action_layer.h

--- tmk_core/common/action.h
+++ tmk_core/common/action.h
@@ -22,6 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "keyboard.h"
 #include "keycode.h"
 #include "action_code.h"
+#include "action_layer.h"
 #include "action_macro.h"
 
 
