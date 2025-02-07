740dbd7c9821440f334543aeb79e71eda99984f7
tmk <hasu@tmk-kbd.com>
2025-02-07 13:39:15 +0900

    news: Fix mapping on keypad enter

--- converter/news_usb/unimap_plain.c
+++ converter/news_usb/unimap_plain.c
@@ -18,7 +18,7 @@ const action_t actionmaps[][UNIMAP_ROWS][UNIMAP_COLS] PROGMEM = {
     GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,BSPC,     INS, HOME,PGUP,    NLCK,PSLS,PAST,PMNS,
     TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,     BSLS,     DEL, END, PGDN,    P7,  P8,  P9,  PPLS,
     CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     GRV, ENT,                         P4,  P5,  P6,  COMM,
-    LSFT,NUBS,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,     RSFT,RSFT,          UP,           P1,  P2,  P3,  L1,
+    LSFT,NUBS,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,     RSFT,RSFT,          UP,           P1,  P2,  P3,  PENT,
     LCTL,LGUI,LALT,MHEN,          SPC,           HENK,NLCK,RALT,RGUI,APP, L1,       LEFT,DOWN,RGHT,         P0,  PDOT,PEQL
     ),
     UNIMAP(
